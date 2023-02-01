42b4cf28754b48fbe590f3c40651d2bf8862eae0
Jeroen van Dijk <jeroentjevandijk@gmail.com>
2023-02-01 14:18:11 +0200

    Demonstrate DB can be used on Clerk Garden

--- notebooks/hello_garden.clj
+++ notebooks/hello_garden.clj
@@ -1,3 +1,23 @@
-(ns hello-garden)
+^{:nextjournal.clerk/toc true
+  :nextjournal.clerk/visibility {:code :hide}}
+(ns hello-garden
+  (:require [next.jdbc :as jdbc]))
 
-(println "hello")
+#_(
+  ;; TODO add script to generate db
+   )
+
+;; ## DB
+
+;; Load hello db (from [Sakila db](https://github.com/jOOQ/sakila/tree/main/sqlite-sakila-db) and [schema here](https://www.jooq.org/sakila).
+
+{:nextjournal.clerk/visibility {:result :hide}}
+
+(def sqlite-db {:dbtype "sqlite" :dbname "./hello.db"})
+(def ds (jdbc/get-datasource sqlite-db))
+
+{:nextjournal.clerk/visibility {:result :show}}
+
+
+;; Run a query
+(jdbc/execute! ds ["select * from customer"])
